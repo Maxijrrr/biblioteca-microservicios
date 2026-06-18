3808af78defe8404849ccaabe04cf0d12c0d52b0
Maxijrrr <max.valenzuelav@duocuc.cl>
2026-06-18 03:14:50 +0000

    Creando ms Gateway (genesis)

---
+++ codigo-fuente/ms-gateway/src/test/java/cl/duoc/ms_gateway/MsGetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_geteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGetewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
